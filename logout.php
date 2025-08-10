6a0cc7d88983a429ad750edf02f37c124e5ecf07
swyajh <dgamee@gmail.com>
2025-08-10 22:40:44 +0700

    first commit

---
+++ logout.php
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+<?php
+session_start();
+
+$_SESSION = array();
+
+session_destroy();
+
+header("Location: login.php");
+exit;
+?>
+=======
+<?php
+session_start();
+
+$_SESSION = array();
+
+session_destroy();
+
+header("Location: login.php");
+exit;
+?>
+>>>>>>> 84d7881f29dca9956c4f8276d1ed835403fbc75e
